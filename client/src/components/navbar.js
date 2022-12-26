d87f2c9dfe4b8c38e4ec495753e7b7c9fc3b82fd
AnithaGowtham1918 <anithagowtham8994@gmail.com>
2022-12-26 12:32:11 +0530

    client setup

---
+++ client/src/components/navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Navbar(props) {
+    return (
+        <>
+        <div className='nav-main'>
+         <div className='nav-left'>BlogSpot</div>
+         <div className='nav-right'>
+            <li>Add Blog</li>
+            <li>Enquiries</li>
+            <li>Notification 
+
+
+
+
+                
+            </li>
+         </div>
+        </div>
+        </>
+    );
+}
+
+export default Navbar;
