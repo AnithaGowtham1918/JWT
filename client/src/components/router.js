c46ce9ee01989e526be4bc0993005203ad1fb1d2
AnithaGowtham1918 <anithagowtham8994@email.com>
2023-03-26 10:20:28 +0530

    new

---
+++ client/src/components/router.js
@@ -0,0 +1,11 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+function Rout(){
+    return(
+     <BrowserRouter>
+     <Routes>
+        <Route></Route>
+     </Routes>
+     </BrowserRouter>
+    )
+}
+export default Rout;
