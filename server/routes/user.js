ff344a6cef2f7bb87371c3aa2430eaaf2dc75af0
AnithaGowtham1918 <anithagowtham8994@gmail.com>
2023-05-10 11:11:52 +0530

    upload

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,41 @@ const router = require("express").Router();
 const createError = require("../utils/error");
 const { updateUser, deleteUser, getUser } = require("../controllers/user");
 //Update user
-router.put("/:id",updateUser);
+router.put("/:id",async(req,res,next)=>{
+    const data = await user.findById(req.params.id);
+        try {
+          const updatedUser = await user.findByIdAndUpdate(req.params.id,{
+              $set:req.body,
+          },{new:true});
+          res.status(200).json(updatedUser);
+        } catch (error) {
+           return next(createError(error.status,error.message));
+        }
+      });
 //delete user
-router.delete("/:id",deleteUser);
+router.delete("/:id",async(req,res,next)=>{
+    if(req.body.userId===req.params.id){
+      try {
+        await user.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted");
+      } catch (error) {
+        return next(createError(404,"User not found"));
+      }
+  
+    }
+    else{
+      return next(createError(405,"You can delete only your account"));
+    }
+  
+  });
 //Get User
-router.get("/:id",getUser);
+router.get("/:id",async(req,res,next)=>{
+    try {
+      const User= await user.findById(req.params.id);
+      res.send(User);
+    } catch (error) {
+      return next(createError(405,"User not found"));
+    }
+});
 //change password
 module.exports=router;
