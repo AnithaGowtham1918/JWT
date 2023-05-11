33d2e197c175f9f41b1df90d89b6e59343cf8ab1
AnithaGowtham1918 <anithagowtham8994@gmail.com>
2023-05-11 09:25:57 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use((error,req,res,next)=>{
 });
 const storage = multer.diskStorage({
     destination:function (req, file, cb){
-      cb(null,path.join(__dirname, ".","public","images"));
+      cb(null,"./public/images");
     },
     filename: function (req, file, cb){
       cb(null,req.body.name);
